7ca0f347fccd82c55c1ab190d4ca5236c0c9e4ed
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-11-04 18:01:37 +0100

    Refactored get_size function into its own file for sharing between reader and writer code

---
+++ ReadWrite/src/utils.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utils.h"
+
+long get_size(char *file_name) {
+    FILE *fp;
+    long size;
+    if ((fp = fopen(file_name, "rb")) == NULL) {
+        err(EXIT_FAILURE, "can't open file '%s'", file_name);
+    }
+    fseek(fp, 0L, SEEK_END);
+    size = ftell(fp);
+    fclose(fp);
+}
