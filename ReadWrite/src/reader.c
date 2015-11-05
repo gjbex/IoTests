f8eb695c7de38b1db73aec142f20de231cff2c51
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-11-05 07:30:05 +0100

    Added unbuffered HDF5 read

--- ReadWrite/src/reader.c
+++ ReadWrite/src/reader.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <hdf5.h>
 
 #include "reader_cl.h"
 #include "utils.h"
@@ -12,6 +13,7 @@ int read_text(char *file_name);
 int read_text_buffered(char *file_name, int buffer_size);
 int read_binary(char *file_name);
 int read_binary_buffered(char *file_name, int buffer_size);
+int read_hdf5(char *file_name);
 int validateCL(Params *params);
 
 int main(int argc, char *argv[]) {
@@ -37,6 +39,8 @@ int main(int argc, char *argv[]) {
             read_text(params.file);
         } else if (strncmp(params.mode, "binary", 6) == 0) {
             read_binary(params.file);
+        } else if (strncmp(params.mode, "hdf5", 4) == 0) {
+            read_hdf5(params.file);
         } else {
             errx(EXIT_FAILURE, "unknown mode '%s' for unbuffered I/O",
                     params.mode);
@@ -155,6 +159,36 @@ int read_binary_buffered(char *file_name, int buffer_size) {
     fprintf(stderr, "%ld double precision numbers read, sum = %le\n",
             nr_reads, sum);
     free(buffer);
+    return EXIT_SUCCESS;
+}
+
+int read_hdf5(char *file_name) {
+    double sum = 0.0;
+    double *data;
+    long i;
+    hid_t file_id, dataset_id, dataspace_id;
+    hsize_t dataspace_dims[1];
+    size_t data_bytes;
+    herr_t status;
+    file_id = H5Fopen(file_name, H5F_ACC_RDONLY, H5P_DEFAULT);
+    dataset_id = H5Dopen(file_id, "/values", H5P_DEFAULT);
+    dataspace_id = H5Dget_space(dataset_id);
+    H5Sget_simple_extent_dims(dataspace_id, dataspace_dims, NULL);
+    data_bytes = dataspace_dims[0]*sizeof(double);
+    if ((data = (double *) malloc(data_bytes)) == NULL) {
+        errx(EXIT_FAILURE, "can't allocate data of size %d", data_bytes);
+    }
+    H5Dread(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL,
+            H5P_DEFAULT, data);
+    H5Dclose(dataset_id);
+    H5Sclose(dataspace_id);
+    H5Fclose(file_id);
+    for (i = 0; i < dataspace_dims[0]; i++) {
+        sum += data[i];
+    }
+    fprintf(stderr, "%lu double precision numbers read, sum = %le\n",
+            (long unsigned) dataspace_dims[0], sum);
+    free(data);
     return EXIT_SUCCESS;
 }
 
